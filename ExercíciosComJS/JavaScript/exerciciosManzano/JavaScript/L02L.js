48f4c827b5ec77023957832012d6a23a9b6f4d06
Vinicius Corrêa Carvalho <163591794+vinilapicos@users.noreply.github.com>
2024-05-22 17:02:24 -0300

    Update L02L.js

--- ExercíciosComJS/JavaScript/exerciciosManzano/JavaScript/L02L.js
+++ ExercíciosComJS/JavaScript/exerciciosManzano/JavaScript/L02L.js
@@ -1,6 +1,6 @@
 let nome = prompt("Digite o seu nome:")
 let sexo = prompt("Digite o seu sexo (M ou F):")
-if (sexo = "F"){
+if (sexo == "F"){
     alert(`Ilma Sra. ${nome}.`)
 } else{
     alert(`Ilmo Sr. ${nome}.`)
